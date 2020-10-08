60f432c46011a163bdbe60e390ac7bbca9cf274f
poojakakuru <poojareddykakuru@gmail.com>
2020-10-08 10:56:43 +0530

    win or tie is added

--- FlipCoin.sh
+++ FlipCoin.sh
@@ -2,23 +2,13 @@
 
 echo "Welcome to FlipCoin simulation program"
 
-coin=$(( RANDOM%2 ))
-echo $coin
-
-if (( $coin==0 ))
-then
-   echo "Head"
-else
-   echo "Tail"
-fi
-
 countofhead=0
 countoftail=0
 
-for (( i=0; i<20; i++ ))
+while [[ $countofhead -le 21 && $countoftail -le 21 ]]
 do
    coin=$(( RANDOM%2 ))
-   if [ $coin==0 ]
+   if [ $coin -eq 0 ]
    then
       echo "Coin flips head"
       countofhead=$(( $countofhead+1 ))
@@ -29,3 +19,13 @@ do
 done
 echo "Number of times head won" $countofhead
 echo "Number of times tail won" $countoftail
+
+if [ $countofhead -gt $countoftail ]
+then
+   echo "Head won by "$(($countofhead-1))
+elif [ $countofhead -le $countoftail ]
+then
+   echo "Tail won by "$(($countoftail-1))
+else
+   echo "It is tie between head and tail"
+fi
